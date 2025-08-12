158ffea498c9aef838954842974d7e71ceec83f4
R Dheepika <69527453+Dheepu2671999@users.noreply.github.com>
2025-08-12 19:08:17 +0530

    Update config.py

--- client-docker/config.py
+++ client-docker/config.py
@@ -3,7 +3,7 @@
 CONFIG = {
     "mcpServers": {
         "local": {
-            "url": "http://mcp-server:8000/mcp/"
+            "url": "http://localhost:8000/mcp/" #if you want to access using the docker add the name of the server docker container  
         }
     }
 }
